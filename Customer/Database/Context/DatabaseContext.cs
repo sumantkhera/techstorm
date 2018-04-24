d8a373e4e9d02981bc71f37df58581a0d66c2310
Sumant Khera <sumantk@damcogroup.com>
2018-04-24 16:30:44 +0530

    code added.

---
+++ Customer/Database/Context/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using Test.Models;
+
+namespace Test.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DefaultConnection") {
+
+        }
+
+        
+    }
+}
---
+++ Customer/Database/Models/CustomerDetails.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database.Models
+{
+    public class CustomerDetails
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public string PrimaryAddress1 { get; set; }
+        public string PrimaryAddress2 { get; set; }
+        public string PrimaryCity { get; set; }
+        public string PrimaryState { get; set; }
+        public int PrimaryZipcode { get; set; }
+
+        public string SecondaryAddress1 { get; set; }
+        public string SecondaryAddress2 { get; set; }
+        public string SecondaryCity { get; set; }
+        public string SecondaryState { get; set; }
+        public int SecondaryZipcode { get; set; }
+    }
+}
