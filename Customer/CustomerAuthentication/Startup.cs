75629903d6d7c3d07a2f2585bbdd50424527a345
mukeshdamco <mukeshc@damcogroup.com>
2018-04-25 16:51:30 +0530

    rahul -Otherization
    rahul -Otherization

---
+++ Customer/CustomerAuthentication/Startup.cs
@@ -0,0 +1,51 @@
+﻿using CustomerAuthentication.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+[assembly: OwinStartup(typeof(CustomerAuthentication.Startup))]
+namespace CustomerAuthentication
+{
+	public class Startup
+	{
+		//public void Configuration(IAppBuilder app)
+		//{
+		//	HttpConfiguration config = new HttpConfiguration();
+		//	WebApiConfig.Register(config);
+		//	app.UseWebApi(config);
+		//}
+
+		public void Configuration(IAppBuilder app)
+		{
+			HttpConfiguration config = new HttpConfiguration();
+			ConfigureOAuth(app);
+			//Rest of code is here;
+			WebApiConfig.Register(config);
+			app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+			//app.UseWebApi(config);
+		}
+
+		public void ConfigureOAuth(IAppBuilder app)
+		{
+			OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+			{
+				AllowInsecureHttp = true,
+				TokenEndpointPath = new PathString("/token"),
+				AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+				Provider = new SimpleAuthorizationServerProvider()
+			};
+
+			// Token Generation
+			app.UseOAuthAuthorizationServer(OAuthServerOptions);
+			app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+		}
+
+	}
+}
