175ae07e189ee6d93e4ae4186f4fbc963bc943fd
Gaurav <Gauravh@Damcogroup.com>
2018-04-25 22:01:27 +0530

    Add Auth

--- Customer/CustomerAuthentication/Providers/SimpleAuthorizationServerProvider.cs
+++ Customer/CustomerAuthentication/Providers/SimpleAuthorizationServerProvider.cs
@@ -6,38 +6,39 @@ using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security.OAuth;
+using Database.Context;
 
 namespace CustomerAuthentication.Providers
 {
-	public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
-	{
-		public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
-		{
-			context.Validated();
-		}
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
 
-		public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
-		{
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
 
-			context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
 
-			using (AuthRepository _repo = new AuthRepository())
-			{
-				IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+            using (var dbcontext = new DatabaseContext())
+            {
+                var user = dbcontext.Users.Where(p => p.UserName == context.UserName && p.Password == context.Password);
 
-				if (user == null)
-				{
-					context.SetError("invalid_grant", "The user name or password is incorrect.");
-					return;
-				}
-			}
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+            }
 
-			var identity = new ClaimsIdentity(context.Options.AuthenticationType);
-			identity.AddClaim(new Claim("sub", context.UserName));
-			identity.AddClaim(new Claim("role", "user"));
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
 
-			context.Validated(identity);
+            context.Validated(identity);
 
-		}
-	}
+        }
+    }
 }
--- Customer/CustomerAuthentication/Startup.cs
+++ Customer/CustomerAuthentication/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using CustomerAuthentication.Providers;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.Infrastructure;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
 using System;
@@ -7,45 +8,128 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Database.Context;
 
 [assembly: OwinStartup(typeof(CustomerAuthentication.Startup))]
 namespace CustomerAuthentication
 {
-	public class Startup
-	{
-		//public void Configuration(IAppBuilder app)
-		//{
-		//	HttpConfiguration config = new HttpConfiguration();
-		//	WebApiConfig.Register(config);
-		//	app.UseWebApi(config);
-		//}
+    public class Startup
+    {
+        //public void Configuration(IAppBuilder app)
+        //{
+        //	HttpConfiguration config = new HttpConfiguration();
+        //	WebApiConfig.Register(config);
+        //	app.UseWebApi(config);
+        //}
 
-		public void Configuration(IAppBuilder app)
-		{
-			HttpConfiguration config = new HttpConfiguration();
-			ConfigureOAuth(app);
-			//Rest of code is here;
-			WebApiConfig.Register(config);
-			app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
-			//app.UseWebApi(config);
-		}
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            ConfigureOAuth(app);
+            //Rest of code is here;
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            //app.UseWebApi(config);
+        }
 
-		public void ConfigureOAuth(IAppBuilder app)
-		{
-			OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
-			{
-				AllowInsecureHttp = true,
-				TokenEndpointPath = new PathString("/token"),
-				AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
-				Provider = new SimpleAuthorizationServerProvider()
-			};
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider(),
+                RefreshTokenProvider = new RefreshTokenProvider()
+            };
 
-			// Token Generation
-			app.UseOAuthAuthorizationServer(OAuthServerOptions);
-			app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
 
-		}
+        }
 
-	}
+    }
+    public class RefreshTokenProvider : IAuthenticationTokenProvider
+    {
+        /// <summary>
+        /// Create refresh token entry in DB
+        /// </summary>
+        /// <param name="context">Authentication Token CreateContext </param>
+        /// <returns>Not returning any thing</returns>
+        public async Task CreateAsync(AuthenticationTokenCreateContext context)
+        {
+            var grant = context.Request.Headers["grant"];
+
+            if (string.IsNullOrEmpty(grant))
+            {
+                return;
+            }
+
+            var refreshTokenId = ApiKeyProvider.GenerateKey();
+            context.Ticket.Properties.Dictionary.Remove("jwtToken");
+            if (grant == "password")
+            {
+                var username = context.Request.Headers["username"];
+                using (var dbcontext = new DatabaseContext())
+                {
+                    var user = dbcontext.Users.Where(p => p.UserName == username).FirstOrDefault();
+
+                    if (user != null)
+                    {
+                        user.Token = refreshTokenId;
+                        user.ModifyOn = DateTime.Now;
+                        await dbcontext.SaveChangesAsync();
+                    }
+                }
+                context.SetToken(refreshTokenId);
+            }
+        }
+
+        /// <summary>
+        /// ReceiveAsync: Called for refresh the token
+        /// </summary>
+        /// <param name="context">Token receive context</param>
+        /// <returns> Not returning any value</returns>
+        public async Task ReceiveAsync(AuthenticationTokenReceiveContext context)
+        {
+
+        }
+
+        /// <summary>
+        /// Create: Not implemented
+        /// </summary>
+        /// <param name="context">AuthenticationTokenCreateContext parameter</param>
+        public void Create(AuthenticationTokenCreateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Receive  Not implemented
+        /// </summary>
+        /// <param name="context">AuthenticationTokenReceiveContext parameter</param>
+        public void Receive(AuthenticationTokenReceiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public static class ApiKeyProvider
+    {
+        /// <summary>
+        /// GenerateKey: used to create a unique key
+        /// </summary>
+        /// <returns>return base64 encoded key</returns>
+        public static string GenerateKey()
+        {
+            var key = new byte[32];
+            RNGCryptoServiceProvider.Create().GetBytes(key);
+            var base64Secret = TextEncodings.Base64Url.Encode(key);
+
+            return base64Secret;
+        }
+    }
 }
