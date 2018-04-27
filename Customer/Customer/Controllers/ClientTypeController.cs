0b3536cfa9f8aff42087460fef658050fc134c85
Gaurav <Gauravh@Damcogroup.com>
2018-04-27 13:39:30 +0530

    Add Modify By

--- Customer/Customer/Controllers/ClientTypeController.cs
+++ Customer/Customer/Controllers/ClientTypeController.cs
@@ -10,7 +10,7 @@ using System.Web.Http;
 
 namespace Customer.Controllers
 {
-    //[CustomAuthorizeAttribute]
+    [CustomAuthorizeAttribute]
     public class ClientTypeController : BaseController
     {
         #region Constructor
